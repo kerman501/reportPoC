7c2e92de47d3893f85b67edddf1d37ca76653ed6
kerman501 <yurigladukhov@gmail.com>
2025-06-06 09:50:19 -0400

    fix qr

--- test2/js/qrScanner.js
+++ test2/js/qrScanner.js
@@ -112,12 +112,7 @@ function setupQrScanner() {
   });
 
   function stopScanner() {
-    startQrScanButton.innerHTML = `
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-qr-code-scan" viewBox="0 0 16 16" style="margin-right: 8px;">
-                <path d="M0 .5A.5.5 0 0 1 .5 0h3a.5.5 0 0 1 0 1h-3A.5.5 0 0 1 0 .5M4 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 4 0m7 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 11 0m3 .5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5M0 4a.5.5 0 0 1 .5-.5H1a.5.5 0 0 1 0 1H.5A.5.5 0 0 1 0 4m3 0a.5.5 0 0 1 .5-.5H4a.5.5 0 0 1 0 1h-.5A.5.5 0 0 1 3 4m1 0h1v1H4zm1 1H4v1h1zm0 1H4v1h1zm1 0h1v1H5zm1 1H5v1h1zm0 1H5v1h1zm1 0h1v1H6zm1 1H6v1h1zm0 1H6v1h1zm1 0h1v1H7zm1 1H7v1h1zm0 1H7v1h1zm1-5h1v1H8zm1 1H8v1h1zm0 1H8v1h1zm1 0h1v1H9zm1 1H9v1h1zm0 1H9v1h1zm1 0h1v1h-1zm1 1h-1v1h1zm0 1h-1v1h1zm1-5h1v1h-1zm1 1h-1v1h1zm0 1h-1v1h1zm1 0h1v1h-1zm1 1h-1v1h1zm0 1h-1v1h1zM15 4a.5.5 0 0 1 .5-.5h.5a.5.5 0 0 1 0 1h-.5a.5.5 0 0 1-.5-.5m-3 0a.5.5 0 0 1 .5-.5H13a.5.5 0 0 1 0 1h-.5a.5.5 0 0 1-.5-.5m1 11.5a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 1 .5-.5m-3 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 1 .5-.5M0 11.5a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 0 11.5m15 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 1 .5-.5M7 4h1v1H7zm1 1H7v1h1zm-1 3h1v1H7zm1 1H7v1h1z"/>
-                <path d="M2 2h2v2H2zM3 3V2H2v1zM2 3h1V2H2zM12 2h2v2h-2zM13 3V2h-1v1zM12 3h1V2h-1zM2 12h2v2H2zM3 13v-1H2v1zM2 13h1v-1H2zM12 12h2v2h-2zM13 13v-1h-1v1zM12 13h1v-1h-1z"/>
-            </svg>
-            Scan QR`; // Restore original button text with SVG
+    startQrScanButton.textContent = "QR";
     startQrScanButton.disabled = false;
     qrScannerContainer.style.display = "none"; // Hide the container for reader and close button
     // qrReaderElement.style.display = 'none'; // Already part of qrScannerContainer
